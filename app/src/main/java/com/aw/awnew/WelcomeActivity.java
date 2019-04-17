fd6b52490a1887bdb0c545f2a812520e965d9eb5
AsteroidWatch <app.asteroidwatch@gmail.com>
2019-04-17 19:22:46 +0100

    Added Navigation, NK

--- app/src/main/java/com/aw/awnew/WelcomeActivity.java
+++ app/src/main/java/com/aw/awnew/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package com.aw.awnew;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switchToLoginActivity();
+        switchToSettingsActivity();
+
+    }
+
+    private void switchToLoginActivity(){
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent (WelcomeActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+
+    private void switchToSettingsActivity(){
+        Button settingsButton = (Button) findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent (WelcomeActivity.this, SettingsActivity.class));
+            }
+        });
+
     }
 }
