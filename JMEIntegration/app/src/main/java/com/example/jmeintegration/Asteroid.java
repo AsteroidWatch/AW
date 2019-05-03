11e14c5c80e7cd9917e73672302562d3a18b46aa
Marko <markocrush96@gmail.com>
2019-05-03 16:37:16 +0100

    android integration

---
+++ JMEIntegration/app/src/main/java/com/example/jmeintegration/Asteroid.java
@@ -0,0 +1,11 @@
+package com.example.jmeintegration;
+
+import com.jme3.scene.Geometry;
+
+public class Asteroid {
+
+	public String id = null;
+	public float size = 0;
+	public Geometry model = null;
+
+}
